2be7448344c79a007031ef2753ac6d96bd9f8bba
Bobby Powers <bobbypowers@gmail.com>
2015-10-02 11:02:53 -0400

    test: add test-cat

---
+++ test/test-cat.ts
@@ -0,0 +1,44 @@
+/// <reference path="../typings/chai/chai.d.ts" />
+/// <reference path="../typings/mocha/mocha.d.ts" />
+
+'use strict';
+
+import * as chai from 'chai';
+import { Boot, Kernel } from '../lib/kernel/kernel';
+
+const expect = chai.expect;
+
+const NODE = 'dist/lib/browser-node/browser-node.js';
+const CAT = '/lib/bin/cat.js';
+
+describe('cat /a', function(): void {
+	const A_CONTENTS = 'contents of a';
+	let kernel: Kernel = null;
+
+	it('should boot', function(done: MochaDone): void {
+		Boot('InMemory', function(err: any, freshKernel: Kernel) {
+			expect(err).to.be.null;
+			expect(freshKernel).not.to.be.null;
+			kernel = freshKernel;
+			done();
+		});
+	});
+
+	it('should create /a', function(done: MochaDone): void {
+		kernel.fs.writeFile('/a', A_CONTENTS, function(err: any) {
+			expect(err).to.be.undefined;
+			done();
+		});
+	});
+
+	it('should run `cat /a`', function(done: MochaDone): void {
+		kernel.system(NODE + ' ' + CAT + ' /a').then(function(result: any) {
+			console.log('run succeeded' + result)
+			done();
+		}).catch(function(reason: any) {
+			console.log('run failed: ' + reason);
+			expect(reason).to.be.null;
+			done();
+		});
+	});
+});

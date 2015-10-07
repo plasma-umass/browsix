01420e7631b0be0fc5a56d3763118181f3e5616e
Bobby Powers <bobbypowers@gmail.com>
2015-10-07 11:55:22 -0700

    test: test cat with 2 files

--- test/test-cat.ts
+++ test/test-cat.ts
@@ -14,10 +14,11 @@ const PREFIX = IS_KARMA ? '/base/' : '';
 const NODE = PREFIX + 'dist/lib/browser-node/browser-node.js';
 const CAT = PREFIX + 'lib/bin/cat.js';
 
-describe('cat /a', function(): void {
+describe('cat /a /b', function(): void {
 	this.timeout(120000);
 
 	const A_CONTENTS = 'contents of a';
+	const B_CONTENTS = 'wish you were here';
 	let kernel: Kernel = null;
 
 	it('should boot', function(done: MochaDone): void {
@@ -36,12 +37,19 @@ describe('cat /a', function(): void {
 		});
 	});
 
-	it('should run `cat /a`', function(done: MochaDone): void {
-		kernel.system(NODE + ' ' + CAT + ' /a', catExited);
+	it('should create /b', function(done: MochaDone): void {
+		kernel.fs.writeFile('/b', B_CONTENTS, function(err: any): void {
+			expect(err).to.be.undefined;
+			done();
+		});
+	});
+
+	it('should run `cat /a /b`', function(done: MochaDone): void {
+		kernel.system(NODE + ' ' + CAT + ' /a /b', catExited);
 		function catExited(code: number, stdout: string, stderr: string): void {
 			try {
 				expect(code).to.equal(0);
-				expect(stdout).to.equal(A_CONTENTS);
+				expect(stdout).to.equal(A_CONTENTS + B_CONTENTS);
 				expect(stderr).to.equal('');
 				done();
 			} catch (e) {

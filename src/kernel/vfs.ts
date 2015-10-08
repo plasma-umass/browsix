4984cc5f0a13924dea40f1425eaa06f8c7d439c7
Bobby Powers <bobbypowers@gmail.com>
2015-10-08 07:18:42 -0400

    include stub vfs

---
+++ src/kernel/vfs.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../typings/node/node.d.ts" />
+/// <reference path="../../typings/browserfs.d.ts" />
+
+'use strict';
+
+import * as BrowserFS from 'browserfs';
+
+// register_filesystem
+// unregister_filesystem
+
+// mount
+
+// different file systems have different file operations, same
+// interface
+
+export interface File {
+
+}

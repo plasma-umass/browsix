fe3a286eb07123e49c38b562ccd5f1a99de4e9c5
Bobby Powers <bobbypowers@gmail.com>
2015-10-16 13:50:08 -0400

    node/lib: remove more instances of let

--- node/fix_paths.sh
+++ node/fix_paths.sh
@@ -3,5 +3,7 @@
 # make all paths relative
 find . -name '*.js' | xargs perl -pi -e "s/require\('/require('.\//"
 
+# remove const + let
 find . -name '*.js' | xargs perl -pi -e 's/^(\s*)const\s/$1var /'
 find . -name '*.js' | xargs perl -pi -e 's/^(\s*)let\s/$1var /'
+find . -name '*.js' | xargs perl -pi -e 's/for\s*\(let\s/for (var /'

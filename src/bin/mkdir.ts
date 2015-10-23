cd5d0a4f0ee2f3d7b89c53dfa5ca040b79e3c5eb
cSg <csgreenberg@cs.umass.edu>
2015-10-23 17:05:24 -0400

    initial version of mkdir -p flag.

--- src/bin/mkdir.ts
+++ src/bin/mkdir.ts
@@ -27,14 +27,33 @@ function main(): void {
 	// the tuple of (path, i) on each iteration.
 	args.map(function(path: string, i: number): void {
 		fs.mkdir(path, (err: any) => {
+			console.log('making path');
 			if (err && pflag) {
 				// TODO: this is where we should check
 				// if path has multiple components,
 				// and if it does, attempt to create
 				// each component.  For now just error
 				// out.
-				process.stderr.write(err.message + '\n', finished);
-				return;
+				//process.stderr.write(err.message + '\n', finished);
+				//return;
+				let subdirs = path.split('/');
+				console.log(subdirs);
+				let subpath = '';
+				for (let j = 0; j < subdirs.length; j++) {
+					subpath += subdirs[j] + '/';
+					fs.stat(subpath, function(oerr: any, stats: fs.Stats): void {
+						// this never runs
+						console.log("I never print");
+						if (oerr) {
+							console.log("ERRR");
+							fs.mkdir(path, (ooerr: any) => {
+								// too much fail. no more try.
+								process.stderr.write(ooerr.message + '\n', finished);
+								return;
+							});
+						}
+					});
+				}
 			} else if (err) {
 				code = 1;
 				process.stderr.write(err.message + '\n', finished);

f50d42f53f85f71c4d0e8a1d0636c406bb6eb942
cSg <csgreenberg@cs.umass.edu>
2015-10-23 03:21:13 -0400

    extended mkdir to take multiple paths and to complain when a path exists.

--- src/bin/mkdir.ts
+++ src/bin/mkdir.ts
@@ -9,15 +9,36 @@ function main(): void {
 
 	let pathToScript = process.argv[1];
 	let args = process.argv.slice(2);
-
-	/*if (args.length && args[0] === '-n') {
-		trailingNewline = false;
+	let pflag = false;
+	if (args.length && args[0] === '-p') {
+		pflag = true;
 		args = args.slice(1);
-	}*/
+	}
 	let code = 0;
-	fs.mkdir(args[0], () => {
-		process.exit(code);
-	});
+	let path: string;
+	for (let i = 0; i < args.length; i ++) {
+		path = args[i];
+		if (pflag) {
+		}
+		else {
+			fs.stat(path, function(err: any, stats: fs.Stats): void {
+				if (err) {
+					// path doesn't exist.  mkdir!
+					fs.mkdir(path, (oerr) => {
+						if (oerr) {
+							code = 1;
+							process.stderr.write(oerr.message);
+							process.exit(code);
+						}
+					});
+				}
+				else {
+					code = 1;
+					process.stderr.write(path + " File exists\n");
+				}
+			});
+		}
+	}
 }
 
 main();

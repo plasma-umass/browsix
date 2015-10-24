b601870c1cf5943bc511ee15dbb98aadd15f4c76
Bobby Powers <bobbypowers@gmail.com>
2015-10-23 21:00:19 -0400

    simplify head, gets unit test passing in Firefox

--- src/bin/head.ts
+++ src/bin/head.ts
@@ -27,42 +27,47 @@ function head(inputs: NodeJS.ReadableStream[], output: NodeJS.WritableStream, nu
 
 	let current = inputs[0];
 	inputs = inputs.slice(1);
-	let n = 0;
+
+	let closed = false;
 	let outstanding = 0;
-	if (!current) {
+
+	function next(): void {
+		closed = true;
+		if (outstanding)
+			return;
 		// use setTimeout to avoid a deep stack as well as
 		// cooperatively yield
 		setTimeout(head, 0, inputs, output, numlines, code);
-		return;
 	}
 
+	function finished(): void {
+		outstanding--;
+		if (!outstanding && closed)
+			return next();
+	}
+
+	if (!current)
+		return next();
+
+	let n = 0;
 	current.on('readable', function(): void {
 		let rl = readline.createInterface({
 			input: current,
 			output: null
 		});
 		rl.on('line', (line: string) => {
-			let shouldClose = false;
-			outstanding++;
 			n++;
 			if (n > numlines) {
 				rl.close();
-				shouldClose = true;
+				next();
 			} else {
-				output.write(line+"\n", () => {
-					outstanding--;
-					if (!outstanding && shouldClose)
-						process.exit(0);
-				});
+				outstanding++;
+				output.write(line+"\n", finished);
 			}
 		});
 	});
 
-	current.on('end', function(): void {
-		// use setTimeout to avoid a deep stack as well as
-		// cooperatively yield
-		setTimeout(head, 0, inputs, output, numlines, code);
-	});
+	current.on('end', next);
 }
 
 function main(): void {
@@ -82,44 +87,40 @@ function main(): void {
 		numlines = +args[1];
 		args = args.slice(2);
 	}
-	if (!args.length) {
-		// no args?  just copy default num lines from stdin to stdout
-		setTimeout(head, 0, [process.stdin], process.stdout, numlines, code);
-	} else {
-		let files: NodeJS.ReadableStream[] = [];
-		let opened = 0;
-		// use map instead of a for loop so that we easily get
-		// the tuple of (path, i) on each iteration.
-		args.map(function(path, i): void {
-			if (path === '-') {
-				files[i] = process.stdin;
-				// if we've opened all of the files, pipe them to
-				// stdout.
-				if (++opened === args.length)
-					setTimeout(head, 0, files, process.stdout, numlines, code);
-				return;
+	if (!args.length)
+		args = ['-'];
+
+	let files: NodeJS.ReadableStream[] = [];
+	let opened = 0;
+	// use map instead of a for loop so that we easily get the
+	// tuple of (path, i) on each iteration.
+	args.map(function(path, i): void {
+		if (path === '-') {
+			files[i] = process.stdin;
+			// if we've opened all of the files, pipe them
+			// to stdout.
+			if (++opened === args.length)
+				setTimeout(head, 0, files, process.stdout, numlines, code);
+			return;
+		}
+		fs.open(path, 'r', function(err: any, fd: any): void {
+			if (err) {
+				// if we couldn't open the specified
+				// file we should print a message but
+				// not exit early - we need to process
+				// as many inputs as we can.
+				files[i] = null;
+				code = 1;
+				process.stderr.write(pathToScript + ': ' + err.message + '\n');
+			} else {
+				files[i] = fs.createReadStream(path, {fd: fd});
 			}
-			fs.open(path, 'r', function(err: any, fd: any): void {
-				if (err) {
-					// if we couldn't open the
-					// specified file we should
-					// print a message but not
-					// exit early - we need to
-					// process as many inputs as
-					// we can.
-					files[i] = null;
-					code = 1;
-					process.stderr.write(pathToScript + ': ' + err.message + '\n');
-				} else {
-					files[i] = fs.createReadStream(path, {fd: fd});
-				}
-				// if we've opened all of the files,
-				// pipe them to stdout.
-				if (++opened === args.length)
-					setTimeout(head, 0, files, process.stdout, numlines, code);
-			});
+			// if we've opened all of the files, pipe them
+			// to stdout.
+			if (++opened === args.length)
+				setTimeout(head, 0, files, process.stdout, numlines, code);
 		});
-	}
+	});
 }
 
 main();

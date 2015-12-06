18659b468e5374be1df516154f9b31e3282ba9ed
Bobby Powers <bobbypowers@gmail.com>
2015-12-06 17:07:34 -0500

    syscall-api: dispatch table

--- src/syscall-api/generate-table.bash
+++ src/syscall-api/generate-table.bash
@@ -3,7 +3,7 @@
 LINUX_SRC="$HOME/src/linux"
 SYSCALLS="$(cat $LINUX_SRC/arch/x86/entry/syscalls/syscall_64.tbl)"
 
-TABLE="$(echo -n "$SYSCALLS" | grep -v 'x32' | grep '^[0-9]' | awk '{print("\tsys_ni_syscall, \\\\ " $1 " " $3)}')"
+TABLE="$(echo -n "$SYSCALLS" | grep -v 'x32' | grep '^[0-9]' | awk '{print("\tsys_ni_syscall, "// $1 " " $3)}')"
 
 cat >table.ts <<EOF
 'use strict';

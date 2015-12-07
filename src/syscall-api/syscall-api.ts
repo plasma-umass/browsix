ead3d0b410e722932526a01bf5e2dda2e1b487eb
Bobby Powers <bobbypowers@gmail.com>
2015-12-07 09:50:00 -0500

    syscall-api: populate argv

--- src/syscall-api/syscall-api.ts
+++ src/syscall-api/syscall-api.ts
@@ -1,5 +1,6 @@
 'use strict';
 
+import { syscall, SyscallCallback, SyscallResponse } from '../browser-node/syscall';
 import { syscallTbl } from './table';
 
 export interface SyscallCallback {
@@ -11,6 +12,47 @@ export function Syscall(cb: Function, trap: number): void {
 };
 
 export var Syscall6 = Syscall;
+
+
+interface Environment {
+	[name: string]: string;
+}
+
+class Process {
+	argv: string[];
+	env: Environment;
+	waiter: Function;
+
+	constructor(argv: string[], environ: Environment) {
+		this.argv = argv;
+		this.env = environ;
+	}
+
+	waitArgv(cb: Function): void {
+		if (this.argv === null)
+			this.waiter = cb;
+		else
+			setTimeout(cb, 0, this.argv);
+	}
+}
+let process = new Process(null, { /* NODE_DEBUG: 'fs' */ });
+(<any>self).process = process;
+
+syscall.addEventListener('init', init.bind(this));
+function init(data: SyscallResponse): void {
+	'use strict';
+
+	let args = data.args[0];
+	let environ = data.args[1];
+	args = [args[0]].concat(args);
+	process.argv = args;
+	process.env = environ;
+	if (process.waiter) {
+		let w = process.waiter;
+		process.waiter = undefined;
+		setTimeout(() => { w(process.argv); }, 0);
+	}
+}
 
 declare var global: any;
 declare var exports: any;

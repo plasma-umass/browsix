f3e940c0791d224fdb7013622b3a4268194b70d0
Bobby Powers <bobbypowers@gmail.com>
2015-12-18 17:11:23 -0500

    add (start of) meme generator service

---
+++ image/main.go
@@ -0,0 +1,111 @@
+// Copyright 2015 The Freetype-Go Authors. All rights reserved.
+// Use of this source code is governed by your choice of either the
+// FreeType License or the GNU General Public License version 2 (or
+// any later version), both of which can be found in the LICENSE file.
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"log"
+	"math"
+	"os"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+var (
+	dpi      = flag.Float64("dpi", 144, "screen resolution in Dots Per Inch")
+	fontfile = flag.String("fontfile", "./impact.ttf", "filename of the ttf font")
+	size     = flag.Float64("size", 24, "font size in points")
+	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
+	wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
+)
+
+const top = "Can't think of an example?"
+const bottom = "Can't think of an example?"
+
+func main() {
+	flag.Parse()
+
+	// Read the font data.
+	fontBytes, err := ioutil.ReadFile(*fontfile)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	f, err := truetype.Parse(fontBytes)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// Draw the background and the guidelines.
+	fg, bg := image.Black, image.White
+	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
+	if *wonb {
+		fg, bg = image.White, image.Black
+		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
+	}
+	const imgW, imgH = 640, 480
+	rgba := image.NewRGBA(image.Rect(0, 0, imgW, imgH))
+	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
+	for i := 0; i < 200; i++ {
+		rgba.Set(10, 10+i, ruler)
+		rgba.Set(10+i, 10, ruler)
+	}
+
+	h := font.HintingFull
+	d := &font.Drawer{
+		Dst: rgba,
+		Src: fg,
+		Face: truetype.NewFace(f, &truetype.Options{
+			Size:    *size,
+			DPI:     *dpi,
+			Hinting: h,
+		}),
+	}
+	y := 10 + int(math.Ceil(*size**dpi/72))
+	dy := int(math.Ceil(*size * *spacing * *dpi / 72))
+	d.Dot = fixed.Point26_6{
+		X: (fixed.I(imgW) - d.MeasureString(top)) / 2,
+		Y: fixed.I(y),
+	}
+	d.DrawString(top)
+	y += dy
+	//for _, s := range text {
+	//	d.Dot = fixed.P(10, y)
+	//	d.DrawString(s)
+	//	y += dy
+	//}
+
+	// Save that RGBA image to disk.
+	outFile, err := os.Create("out.png")
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	defer outFile.Close()
+	b := bufio.NewWriter(outFile)
+	err = png.Encode(b, rgba)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	err = b.Flush()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println("Wrote out.png OK.")
+	os.Exit(0)
+}

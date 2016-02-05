887c38ab5d0cd9e9b939a42a8f51a41c4079bc11
Bobby Powers <bobbypowers@gmail.com>
2016-02-05 13:51:56 -0500

    background for service

--- meme-service/main.go
+++ meme-service/main.go
@@ -7,11 +7,12 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"flag"
 	"fmt"
 	"image"
-	"image/color"
 	"image/draw"
+	_ "image/jpeg"
 	"image/png"
 	"io/ioutil"
 	"log"
@@ -19,23 +20,33 @@ import (
 	"os"
 
 	"github.com/golang/freetype/truetype"
+	"github.com/nfnt/resize"
 	"golang.org/x/image/font"
 	"golang.org/x/image/math/fixed"
 )
 
 var (
 	dpi      = flag.Float64("dpi", 144, "screen resolution in Dots Per Inch")
-	fontfile = flag.String("fontfile", "./impact.ttf", "filename of the ttf font")
-	size     = flag.Float64("size", 24, "font size in points")
+	fontfile = flag.String("fontfile", "./font/leaguegothic-regular-webfont.ttf", "filename of the ttf font")
+	size     = flag.Float64("size", 36, "font size in points")
 	spacing  = flag.Float64("spacing", 1.5, "line spacing (e.g. 2 means double spaced)")
-	wonb     = flag.Bool("whiteonblack", false, "white text on a black background")
 )
 
-const top = "Can't think of an example?"
-const bottom = "Can't think of an example?"
+const bgImgSrc = "./img/Futurama-Zoidberg.jpg"
+
+const top = "Can't think of a demo?"
+const bottom = "Why not Zoidberg?"
+
+const imgW, imgH = 640, 480
 
 func main() {
 	flag.Parse()
+
+	// read-in images
+
+	// read-in font
+
+	// start http server
 
 	// Read the font data.
 	fontBytes, err := ioutil.ReadFile(*fontfile)
@@ -49,20 +60,23 @@ func main() {
 		return
 	}
 
-	// Draw the background and the guidelines.
-	fg, bg := image.Black, image.White
-	ruler := color.RGBA{0xdd, 0xdd, 0xdd, 0xff}
-	if *wonb {
-		fg, bg = image.White, image.Black
-		ruler = color.RGBA{0x22, 0x22, 0x22, 0xff}
+	bgImgBytes, err := ioutil.ReadFile(bgImgSrc)
+	if err != nil {
+		log.Println(err)
+		return
 	}
-	const imgW, imgH = 640, 480
+
+	bgImg, _, err := image.Decode(bytes.NewReader(bgImgBytes))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	// resize to match our output dimensions
+	bgImg = resize.Resize(imgW, imgH, bgImg, resize.Lanczos3)
+
+	fg := image.Black
 	rgba := image.NewRGBA(image.Rect(0, 0, imgW, imgH))
-	draw.Draw(rgba, rgba.Bounds(), bg, image.ZP, draw.Src)
-	for i := 0; i < 200; i++ {
-		rgba.Set(10, 10+i, ruler)
-		rgba.Set(10+i, 10, ruler)
-	}
+	draw.Draw(rgba, rgba.Bounds(), bgImg, image.ZP, draw.Src)
 
 	h := font.HintingFull
 	d := &font.Drawer{
@@ -82,6 +96,14 @@ func main() {
 	}
 	d.DrawString(top)
 	y += dy
+
+	y = imgH - 15
+	d.Dot = fixed.Point26_6{
+		X: (fixed.I(imgW) - d.MeasureString(bottom)) / 2,
+		Y: fixed.I(y),
+	}
+	d.DrawString(bottom)
+
 	//for _, s := range text {
 	//	d.Dot = fixed.P(10, y)
 	//	d.DrawString(s)

a1c32648ff273702e86e7b91685dc9b6566c18df
Bobby Powers <bobbypowers@gmail.com>
2016-02-23 11:40:03 -0500

    bin/ls: cleanup a bit

--- src/bin/ls.ts
+++ src/bin/ls.ts
@@ -5,19 +5,14 @@
 import * as fs from 'fs';
 import {format} from 'util';
 
-interface ArgHandlers {
-	[n: string]: Function;
-}
-
 function log(fmt: string, ...args: any[]): void {
 	let cb: Function = undefined;
 	if (args.length && typeof args[args.length-1] === 'function') {
 		cb = args[args.length-1];
 		args = args.slice(0, -1);
 	}
-	let pathToScript = process.argv[1];
-	let msg = format.apply(undefined, [fmt].concat(args));
-	let err = format('%s: %s\n', pathToScript, msg);
+	let prog = process.argv[1].split('/').slice(-1);
+	let msg = prog + ': ' + format.apply(undefined, [fmt].concat(args)) + '\n';
 
 	if (cb)
 		process.stderr.write(err, cb);
@@ -25,9 +20,10 @@ function log(fmt: string, ...args: any[]): void {
 		process.stderr.write(err);
 }
 
-function parseArgs(args: string[], handlers: ArgHandlers): [string[], boolean] {
-	let positionalArgs: string[] = [];
+function parseArgs(args: string[], handlers: {[n: string]: Function}): [string[], boolean] {
 	let ok = true;
+	let positionalArgs: string[] = args.filter((arg) => arg.substring(0, 1) !== '-');
+	args = args.filter((arg) => arg.substring(0, 1) === '-');
 
 	let errs = 0;
 	function done(): void {
@@ -42,14 +38,17 @@ function parseArgs(args: string[], handlers: ArgHandlers): [string[], boolean] {
 		// append our own callback.
 		log.apply(this, args.concat([done]));
 	}
+	function usage(): void {
+		errs++;
+		let prog = process.argv[1].split('/').slice(-1);
+		let flags = Object.keys(handlers).sort().join('');
+		if (flags)
+			flags = format(' [-%s]', flags);
+		let msg = format('usage: %s%s ARGS\n', prog, flags);
+		process.stderr.write(msg, done);
+	}
 
 	for (let i = 0; i < args.length; i++) {
-		if (args[i].substring(0, 1) !== '-') {
-			positionalArgs.push(args[i]);
-			continue;
-		}
-
-		// all args are a single-character long
 		let argList = args[i].slice(1);
 		if (argList.length && argList[0] === '-') {
 			error('unknown option "%s"', args[i]);
@@ -57,29 +56,25 @@ function parseArgs(args: string[], handlers: ArgHandlers): [string[], boolean] {
 		}
 		for (let j = 0; j < argList.length; j++) {
 			let arg = argList[j];
-			let handler = handlers[arg];
-			if (handler) {
-				handler();
-			} else {
+			if (handlers[arg])
+				handlers[arg]();
+			else
 				error('invalid option "%s"', arg);
-			}
 		}
 	}
+
+	if (!ok) usage();
 
 	return [positionalArgs, ok];
 }
 
 function main(): void {
-	'use strict';
-
-	let pathToScript = process.argv[1];
-
 	let all: boolean = false;
 
 	let [args, ok] = parseArgs(
 		process.argv.slice(2),
 		{
-			'l': (): any => {},
+			'l': (): any => {}, // nop, but make sure it doesn't error out
 			'a': (): any => all = true,
 		}
 	);

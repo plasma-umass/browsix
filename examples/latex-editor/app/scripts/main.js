18b93cd7666a5c827c463cfe39613d78c7ee91e2
Bobby Powers <bobbypowers@gmail.com>
2016-05-10 02:33:02 -0400

    add latex example

---
+++ examples/latex-editor/app/scripts/main.js
@@ -0,0 +1,78 @@
+(() => {
+	'use strict';
+	let f = 'example';
+	let texFile = f + '.tex';
+	let bibFile = 'mybib.bib';
+	let edTex = document.getElementById('ed-tex');
+	let edBib = document.getElementById('ed-bib');
+	let button = document.getElementById('create-button');
+	let pdfEmbed = document.getElementById('pdf-embed');
+	let kernel = null;
+	function startBrowsix() {
+		window.Boot(
+			'XmlHttpRequest',
+			['index.json', 'fs', true],
+			(err, k) => {
+				if (err) {
+					console.log(err);
+					throw new Error(err);
+				}
+				kernel = k;
+				loadFiles();
+			});
+	}
+	function loadFiles() {
+		edTex.value = kernel.fs.readFileSync(texFile).toString();
+		edBib.value = kernel.fs.readFileSync(bibFile).toString();
+
+		button.disabled = false;
+	}
+	function saveFiles(next) {
+		kernel.fs.writeFile(texFile, edTex.value, () => {
+			kernel.fs.writeFile(bibFile, edBib.value, () => {
+				next();
+			});
+		});
+	}
+	function showPDF() {
+		var fName = f + '.pdf';
+		var mimeType = 'application/pdf';
+		var buf = new Uint8Array(kernel.fs.readFileSync(fName).data.buff.buffer);
+		var blob = new Blob([buf], {type: mimeType});
+		pdfEmbed.src = window.URL.createObjectURL(blob);
+
+		$(button).toggleClass('is-active').blur();
+	}
+	let sequence = [
+		'pdflatex ' + f,
+		'bibtex ' + f,
+		'pdflatex ' + f,
+		'pdflatex ' + f,
+	];
+	function runLatex() {
+		let seq = sequence.slice();
+		function onStdout(pid, out) {
+			console.log(out);
+		}
+		function onStderr(pid, out) {
+			console.log(out);
+		}
+		function runNext(pid, code) {
+			if (code !== 0)
+				alert('latex failed');
+			var cmd = seq.shift();
+			if (!cmd) {
+				showPDF();
+				return;
+			}
+			kernel.system(cmd, runNext, onStdout, onStderr);
+		}
+		runNext(-1, 0);
+	}
+	function clicked() {
+		$(button).toggleClass('is-active').blur();
+		saveFiles(runLatex);
+	}
+	button.addEventListener('click', clicked);
+	startBrowsix();
+})();
---
+++ examples/latex-editor/gulpfile.babel.js
@@ -0,0 +1,194 @@
+// generated on 2016-02-11 using generator-gulp-webapp 1.1.1
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+import {stream as wiredep} from 'wiredep';
+import proxy from 'proxy-middleware';
+import rename from 'gulp-rename';
+import run from 'gulp-run';
+import url from 'url';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+gulp.task('styles', () => {
+  return gulp.src('app/styles/*.scss')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: 'expanded',
+      precision: 10,
+      includePaths: ['.']
+    }).on('error', $.sass.logError))
+    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', () => {
+  return gulp.src('app/scripts/**/*.js')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('.tmp/scripts'))
+    .pipe(reload({stream: true}));
+});
+
+function lint(files, options) {
+  return () => {
+    return gulp.src(files)
+      .pipe(reload({stream: true, once: true}))
+//      .pipe($.eslint(options))
+      .pipe($.eslint.format())
+      .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+  };
+}
+const testLintOptions = {
+  env: {
+    mocha: true
+  }
+};
+
+gulp.task('lint', lint('app/scripts/**/*.js', {rules: {'no-use-before-define': [2, {functions: false}]}}));
+gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
+
+gulp.task('html', ['styles', 'scripts'], () => {
+  return gulp.src('app/*.html')
+    .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
+    .pipe($.if('*.js', $.uglify()))
+    .pipe($.if('*.css', $.cssnano()))
+    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('images', () => {
+  return gulp.src('app/images/**/*')
+    .pipe($.if($.if.isFile, $.cache($.imagemin({
+      progressive: true,
+      interlaced: true,
+      // don't remove IDs from SVGs, they are often used
+      // as hooks for embedding and styling
+      svgoPlugins: [{cleanupIDs: false}]
+    }))
+    .on('error', function (err) {
+      console.log(err);
+      this.end();
+    })))
+    .pipe(gulp.dest('dist/images'));
+});
+
+gulp.task('fonts', () => {
+  return gulp.src('app/fonts/**/*')
+    .pipe(gulp.dest('.tmp/fonts'))
+    .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('extras', () => {
+  return gulp.src([
+    'app/*.*',
+    '!app/*.html'
+  ], {
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+// finally, we create an index.json file so that BrowserFS can see
+// everything in our nice hierarchy
+gulp.task('index-fs', /*['build-fs'],*/ function() {
+    return run('./xhrfs-index fs').exec()
+        .pipe(rename(function(path) {
+            path.basename = 'index';
+            path.extname = '.json';
+        }))
+        .pipe(gulp.dest('./fs'));
+});
+
+gulp.task('serve', ['styles', 'scripts', 'fonts', 'index-fs'], () => {
+  let proxyOptions = url.parse('http://localhost:8080/api');
+  proxyOptions.route = '/api';
+
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['.tmp', 'app'],
+      routes: {
+        '/node_modules': 'node_modules',
+        '/bower_components': 'bower_components',
+        '/fs': 'fs',
+      },
+      middleware: [proxy(proxyOptions)],
+    }
+  });
+
+  gulp.watch([
+    'app/*.html',
+    '.tmp/scripts/**/*.js',
+    'app/images/**/*',
+    '.tmp/fonts/**/*'
+  ]).on('change', reload);
+
+  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('app/fonts/**/*', ['fonts']);
+  gulp.watch('bower.json', ['wiredep', 'fonts']);
+});
+
+gulp.task('serve:dist', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: ['dist']
+    }
+  });
+});
+
+gulp.task('serve:test', ['scripts'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    ui: false,
+    server: {
+      baseDir: 'test',
+      routes: {
+        '/scripts': '.tmp/scripts',
+        '/bower_components': 'bower_components',
+        '/node_modules': 'node_modules',
+      }
+    }
+  });
+
+  gulp.watch('app/scripts/**/*.js', ['scripts']);
+  gulp.watch('test/spec/**/*.js').on('change', reload);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
+});
+
+// inject bower components
+gulp.task('wiredep', () => {
+  gulp.src('app/styles/*.scss')
+    .pipe(wiredep({
+      ignorePath: /^(\.\.\/)+/
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/*.html')
+    .pipe(wiredep({
+      exclude: ['bootstrap-sass'],
+      ignorePath: /^(\.\.\/)*\.\./
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras', 'index-fs'], () => {
+  return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
+});

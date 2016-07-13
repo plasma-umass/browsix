f4642a101cf8c3e3d49b17032676725c778537ec
Bobby Powers <bobbypowers@gmail.com>
2016-07-13 10:36:29 -0400

    examples/meme-service: update deps + don't minify

--- examples/meme-service/frontend/gulpfile.babel.js
+++ examples/meme-service/frontend/gulpfile.babel.js
@@ -58,9 +58,9 @@ gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 gulp.task('html', ['styles', 'scripts'], () => {
   return gulp.src('app/*.html')
     .pipe($.useref({searchPath: ['.tmp', 'app', '.']}))
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.cssnano()))
-    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
+//    .pipe($.if('*.js', $.uglify()))
+//    .pipe($.if('*.css', $.cssnano()))
+//    .pipe($.if('*.html', $.htmlmin({collapseWhitespace: true})))
     .pipe(gulp.dest('dist'));
 });
 

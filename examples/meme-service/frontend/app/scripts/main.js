108e4e214896b5646da3b55f33dfdfdc198998b2
Bobby Powers <bobbypowers@gmail.com>
2016-08-18 23:11:37 -0400

    meme-sevice: disable service worker for now

--- examples/meme-service/frontend/app/scripts/main.js
+++ examples/meme-service/frontend/app/scripts/main.js
@@ -206,20 +206,20 @@
 	button.addEventListener('click', clicked);
 
 	window.onload = () => {
-		if ('serviceWorker' in navigator) {
-			navigator.serviceWorker.register('/sw.js?v7', { scope: '/' }).then(function(reg) {
+		// if ('serviceWorker' in navigator) {
+		// 	navigator.serviceWorker.register('/sw.js?v7', { scope: '/' }).then(function(reg) {
 
-				if(reg.installing)
-					console.log('Service worker installing');
-				else if(reg.waiting)
-					console.log('Service worker installed');
-				else if(reg.active)
-					console.log('Service worker active');
+		// 		if(reg.installing)
+		// 			console.log('Service worker installing');
+		// 		else if(reg.waiting)
+		// 			console.log('Service worker installed');
+		// 		else if(reg.active)
+		// 			console.log('Service worker active');
 
-			}).catch(function(error) {
-				// registration failed
-				console.log('Registration failed with ' + error);
-			});
-		};
+		// 	}).catch(function(error) {
+		// 		// registration failed
+		// 		console.log('Registration failed with ' + error);
+		// 	});
+		// };
 	};
 })();

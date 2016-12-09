b0337a579d115e9dfc081e1eb1bb5ad5cbf570b2
Bobby Powers <bobbypowers@gmail.com>
2016-12-09 09:48:26 -0500

    examples: go-wait to test waiting on child processes

---
+++ examples/go-wait/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func main() {
+	cmd := exec.Command("sleep", "5")
+	err := cmd.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Waiting for command to finish...")
+	err = cmd.Wait()
+	log.Printf("Command finished with error: %v", err)
+}

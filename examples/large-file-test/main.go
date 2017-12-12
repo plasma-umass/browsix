35f50c0fb89db7dd064b8be722b7c0f4d94b36c3
Bobby Powers <bobbypowers@gmail.com>
2017-12-12 16:23:34 -0500

    examples/large-file-test: clean up logic, add flags

--- examples/large-file-test/main.go
+++ examples/large-file-test/main.go
@@ -1,69 +1,71 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path"
+
+	"github.com/gin-gonic/gin"
 )
 
+var (
+	addr   = flag.String("addr", "128.0.0.1:80", "address to listen on")
+	prefix = flag.String("prefix", "/srv", "large file location")
+)
+
+func read(filename string) string {
+	fullPath := path.Join(*prefix, filename)
+
+	f, err := os.Open(fullPath)
+	if err != nil {
+		panic(fmt.Errorf("Open(%s): %s", fullPath, err))
+	}
+	defer f.Close()
+
+	contents, err := ioutil.ReadAll(f)
+	if err != nil {
+		panic(fmt.Errorf("ReadAll(%s): %s", fullPath, err))
+	}
+
+	return string(contents)
+}
+
 func main() {
+	flag.Parse()
+
 	router := gin.Default()
 
-	file1, _ := os.Open("/usr/bin/big1.txt")
-	file2, _ := os.Open("/usr/bin/big2.txt")
-	file3, _ := os.Open("/usr/bin/big3.txt")
-	file4, _ := os.Open("/usr/bin/big4.txt")
-	file5, _ := os.Open("/usr/bin/big5.txt")
-
-	/*
-	file1, _ := os.Open("big1.txt")
-	file2, _ := os.Open("big2.txt")
-	file3, _ := os.Open("big3.txt")
-	file4, _ := os.Open("big4.txt")
-	file5, _ := os.Open("big5.txt")
-	*/
-
-	defer file1.Close()
-	defer file2.Close()
-	defer file3.Close()
-	defer file4.Close()
-	defer file5.Close()
-
-	b1, _ := ioutil.ReadAll(file1)
-	b2, _ := ioutil.ReadAll(file2)
-	b3, _ := ioutil.ReadAll(file3)
-	b4, _ := ioutil.ReadAll(file4)
-	b5, _ := ioutil.ReadAll(file5)
-
-	fileString1 := string(b1)
-	fileString2 := string(b2)
-	fileString3 := string(b3)
-	fileString4 := string(b4)
-	fileString5 := string(b5)
+	contents1 := read("big1.txt")
+	contents2 := read("big2.txt")
+	contents3 := read("big3.txt")
+	contents4 := read("big4.txt")
+	contents5 := read("big5.txt")
 
 	router.GET("/big1", func(c *gin.Context) {
-		c.String(200, fileString1)
+		c.String(200, contents1)
 	})
 
 	router.GET("/big2", func(c *gin.Context) {
-		c.String(200, fileString2)
+		c.String(200, contents2)
 	})
 
 	router.GET("/big3", func(c *gin.Context) {
-		c.String(200, fileString3)
+		c.String(200, contents3)
 	})
 
 	router.GET("/big4", func(c *gin.Context) {
-		c.String(200, fileString4)
+		c.String(200, contents4)
 	})
 
 	router.GET("/big5", func(c *gin.Context) {
-		c.String(200, fileString5)
+		c.String(200, contents5)
 	})
 
 	router.GET("/test", func(c *gin.Context) {
 		c.String(200, "this is a test\n")
 	})
 
-	router.Run("128.0.0.1:8080")
+	router.Run(*addr)
 }

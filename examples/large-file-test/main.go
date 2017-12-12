0787b2e50c2601ef0e5d97f3fa479a9a0054abba
SamGinzburg <ginzburg.sam@gmail.com>
2017-12-12 13:52:48 -0500

    updated benchmarking code

--- examples/large-file-test/main.go
+++ examples/large-file-test/main.go
@@ -3,35 +3,67 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"io/ioutil"
-	"fmt"
 	"os"
 )
 
 func main() {
 	router := gin.Default()
-
-    file, err1 := os.Open("/usr/bin/big.txt")
 	
-	if err1 != nil {
-        fmt.Fprintf(os.Stderr, "ERR: %s\n\n", err1)
-    }
-    defer file.Close()
+	file1, _ := os.Open("/usr/bin/big1.txt")
+	file2, _ := os.Open("/usr/bin/big2.txt")
+	file3, _ := os.Open("/usr/bin/big3.txt")
+	file4, _ := os.Open("/usr/bin/big4.txt")
+	file5, _ := os.Open("/usr/bin/big5.txt")
+	
+	/*
+	file1, _ := os.Open("big1.txt")
+	file2, _ := os.Open("big2.txt")
+	file3, _ := os.Open("big3.txt")
+	file4, _ := os.Open("big4.txt")
+	file5, _ := os.Open("big5.txt")
+	*/
 
-    b, err := ioutil.ReadAll(file)
+	defer file1.Close()
+	defer file2.Close()
+	defer file3.Close()
+	defer file4.Close()
+	defer file5.Close()
 
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "ERR: %s\n\n", err)
-	}
+	b1, _ := ioutil.ReadAll(file1)
+	b2, _ := ioutil.ReadAll(file2)
+	b3, _ := ioutil.ReadAll(file3)
+	b4, _ := ioutil.ReadAll(file4)
+	b5, _ := ioutil.ReadAll(file5)
 
-	fileString := string(b)
-	router.GET("/big", func(c *gin.Context) {
-		c.String(200, fileString)
+	fileString1 := string(b1)
+	fileString2 := string(b2)
+	fileString3 := string(b3)
+	fileString4 := string(b4)
+	fileString5 := string(b5)
+
+	router.GET("/big1", func(c *gin.Context) {
+		c.String(200, fileString1)
+	})
+
+	router.GET("/big2", func(c *gin.Context) {
+		c.String(200, fileString2)
+	})
+
+	router.GET("/big3", func(c *gin.Context) {
+		c.String(200, fileString3)
+	})
+
+	router.GET("/big4", func(c *gin.Context) {
+		c.String(200, fileString4)
+	})
+
+	router.GET("/big5", func(c *gin.Context) {
+		c.String(200, fileString5)
 	})
 
 	router.GET("/test", func(c *gin.Context) {
 		c.String(200, "this is a test\n")
 	})
 
-
-	router.Run("128.0.0.1:8080")
+	router.Run("127.0.0.1:8080")
 }
